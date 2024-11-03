65393bf7c955d91d45852b4e9ac6259b55a8b5fd
Ville Davidsson <ville.d@hotmail.com>
2024-11-03 23:29:50 +0200

    unit tests to 100% branch coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,42 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ei_voi_ottaa_enemman_kuin_on(self):
+        self.varasto.lisaa_varastoon(6)
+
+        maara = self.varasto.ota_varastosta(8)
+
+        self.assertAlmostEqual(maara, 6)
+
+    def test_ei_voi_laittaa_enemman_kuin_tilaa(self):
+        self.varasto.lisaa_varastoon(20)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ei_lisata_negatiivista_tilaa(self):
+        self.varasto.lisaa_varastoon(-5)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_ei_oteta_negatiivista_tilaa(self):
+        maara = self.varasto.ota_varastosta(-5)
+
+        self.assertAlmostEqual(maara, 0.0)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ei_luoda_negatiivista_varastoa(self):
+        self.negvarasto = Varasto(-5, -5)
+
+        self.assertAlmostEqual(self.negvarasto.tilavuus, 0)
+        self.assertAlmostEqual(self.negvarasto.saldo, 0)
+
+    def test_ei_luoda_ylitse_tayttyvaa_varastoa(self):
+        self.ylivarasto = Varasto(5, 10)
+
+        self.assertAlmostEqual(self.ylivarasto.saldo, 5)
+
+    def test_tulostaa_oikein(self):
+        self.assertAlmostEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
+
+
